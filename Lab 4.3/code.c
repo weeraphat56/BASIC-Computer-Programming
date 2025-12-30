b5249bce978b44d2704eb4acfe4f27e34488f9cb
weerapat boonma <weerapatboonma44@gmail.com>
2025-12-30 23:28:16 +0700

    Add files via upload

--- Lab 4.3/code.c
+++ Lab 4.3/code.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 
-struct Product {
-    char name[ 50 ] ;
-    int productId ;
-    float price ;
-};
-
 int main() {
+    int DAYS = 7 ;
 
-    struct Product item ;
+    int daily_temp[ DAYS ] ;
+    int max_temp ;
+    int i ;
 
-    if ( scanf( "%d %f %s" , &item.productId , &item.price , item.name ) != 3 ) {
-        return 1 ;
+    for ( i = 0 ; i < DAYS ; i++ ) {
+        printf( "Enter temperature for Day %d: " , i + 1 ) ;
+        if ( scanf( "%d" , &daily_temp[ i ] ) != 1 ) {
+            return 1 ;
+        }// end if
+    }// end for loop
+
+    max_temp = daily_temp[ 0 ] ;
+
+    for ( i = 0 ; i < DAYS ; i++ ) {
+        if ( daily_temp[ i ] > max_temp ) {
+            max_temp = daily_temp[ i ] ;
+        }// end if
+    }// end for loop
+
+    printf( "\n--- DAILY TEMPERATURE REPORT ---\n") ;
+    printf( "Recorded Temperatures (C): ") ;
+
+    for ( i = 0 ; i < DAYS ; i++ ) {
+        printf( "%d " , daily_temp[ i ] ) ;
+    }// end for loop
+
+    printf( "\nMaximum Tempareture Found: %d (C)\n" , max_temp ) ;
+
+    if ( max_temp >= 35 ) {
+        printf( "Weather is HOT!" ) ;
     }
-
-    printf( "ID: %d, Price: %.2f, Name: %s" , item.productId , item.price , item.name ) ;
+    else {
+        printf( "Weather is MODERATE!" ) ;
+    }// end if else
 
     return 0 ;
-}//end main function
+}// end main function
