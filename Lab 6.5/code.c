ef2a9fe372c8f4c8f1c334fa8f3e9f5f82a1253d
weerapat boonma <weerapatboonma44@gmail.com>
2025-12-31 00:25:03 +0700

    Add files via upload

--- Lab 6.5/code.c
+++ Lab 6.5/code.c
@@ -1,60 +1,46 @@
 #include <stdio.h>
 
-#define SIZE 5
+#define MAX_SIZE 50
 
-int calculate_net_balance(int *trans_array, int size, int *status_ptr);
+int count_vowels_with_pointer( char *str ) ;
 
-int main () {
-    int transactions[ SIZE ] ;
-    int net_balance ;
-    int finance_status ;
-    int i ;
+int main() {
+    char sentence[ MAX_SIZE ] ;
+    int vowel_count ;
 
-    printf( "Enter %d transactions (Income +, Expense -):\n" , SIZE ) ;
-    for ( i = 0 ; i < SIZE ; i++ ) {
-        printf( "Transaction %d: " , i + 1 ) ;
-        if ( scanf( "%d" , &transactions[i]) != 1 ) {
-            return 1 ;
-        }
-    }
+    printf( "Enter a sentence: " ) ;
 
-    net_balance = calculate_net_balance( transactions , SIZE , &finance_status ) ;
+    if ( fgets( sentence , MAX_SIZE , stdin ) == NULL) {
+        return 1 ;
+    }// end if
 
-    printf( "\n--- PERSONAL FINANCE REPORT ---\n" ) ;
-    printf( "Transactions Recorded: " ) ;
+    sentence[ strcspn( sentence , "\n" ) ] = '\0' ;
 
-    for ( i = 0 ; i < SIZE ; i++ ) {
-        printf( "%d " , transactions[i] ) ;
-    }
+    vowel_count = 0 ;
+    char *char_ptr = sentence ;
 
-    printf( "\n" ) ;
-    printf( "Net Balance: %d\n" , net_balance ) ;
-    printf( "Status: " ) ;
+    vowel_count = count_vowels_with_pointer( char_ptr ) ;
 
-    if ( finance_status == 1 ) {
-        printf( "PROFIT" ) ;
-    }
-    else {
-        printf( "LOSS" ) ;
-    }
+    printf( "\n--- STRING ANALYSIS REPORT ---\n" ) ;
+    printf( "Input String: \"%s\"\n" , sentence ) ;
+    printf( "Total Vowel Count: %d\n" , vowel_count ) ;
 
     return 0 ;
 }// end main function
 
-int calculate_net_balance( int *trans_array , int size , int *status_ptr ) {
-    int net = 0 ;
-    int i ;
+int count_vowels_with_pointer( char *str ) {
+    int counter = 0 ;
+    int i = 0 ;
+    
+    while ( *(str + i) != '\0' ) {
+        if ( *(str+i) == 'a' || *(str+i) == 'e' || *(str+i) == 'i' || *(str+i) == 'o' || *(str+i) == 'u' ) {
+            counter++ ;
+        }
+        else if ( *(str+i) == 'A' || *(str+i) == 'E' || *(str+i) == 'I' || *(str+i) == 'O' || *(str+i) == 'U' ) {
+            counter++ ;
+        }// end if else
+        i++ ;
+    }// end while loop
 
-    for ( i = 0 ; i < size ; i++ ) {
-        net += *( trans_array + i ) ;
-    }
-
-    if ( net < 0 ) {
-        *status_ptr = 0 ;
-    }
-    else {
-        *status_ptr = 1 ;
-    }
-
-    return net ;
-}// end calculate_net_balance function
+    return counter ;
+}// end count_vowels_with_pointer function
